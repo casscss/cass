e2223b3acfa11d6c6800f89dd1ff3da802caabd8
Rex Riepe <rexriepe@gmail.com>
2022-09-01 19:39:13 -0700

    color picker support on homepage

--- javascript/css-page.js
+++ javascript/css-page.js
@@ -298,6 +298,8 @@ function updateColor() {
 
     var rest_css = deleteLines(original_css, 8);
 
+    var rgb_color = hslToRgb((h/360), (s/100), (l/100));
+
     if (l > 60) {
         document.getElementById('added_theme').innerHTML = css_template;
         document.getElementById('css_textarea').innerHTML = css_header + rest_css;
@@ -305,12 +307,105 @@ function updateColor() {
     } else {
         document.getElementById('added_theme').innerHTML = css_template_alt;
         document.getElementById('css_textarea').innerHTML = css_header_alt + rest_css;
-
-
     }
 
-
-
-    
+    document.getElementById("baseColor").value = rgb_color;
 
 }
+
+function updateColorRGB() {
+    var rgb_hex = document.getElementById("baseColor").value;
+    var rgb_color = hexToRgb(rgb_hex);
+    var hsl_color = rgbToHsl(rgb_color.r, rgb_color.g, rgb_color.b);
+
+    var h = hsl_color[0] * 360;
+    var s = hsl_color[1] * 100;
+    var l = hsl_color[2] * 100;
+
+    document.getElementById("baseHue").value = h;
+    document.getElementById("baseSaturation").value = s;
+    document.getElementById("baseLightness").value = l;
+
+    document.getElementById("baseHueLabel").innerHTML = Math.round(h);
+    document.getElementById("baseSaturationLabel").innerHTML = Math.round(s);
+    document.getElementById("baseLightnessLabel").innerHTML = Math.round(l);
+
+    updateColor();
+}
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   {number}  h       The hue
+ * @param   {number}  s       The saturation
+ * @param   {number}  l       The lightness
+ * @return  {Array}           The RGB representation
+ */
+ function hslToRgb(h, s, l){
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        var hue2rgb = function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return rgbToHex(Math.round(r * 255), Math.round(g * 255), Math.round(b * 255));
+}
+
+/**
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * @param   {number}  r       The red color value
+ * @param   {number}  g       The green color value
+ * @param   {number}  b       The blue color value
+ * @return  {Array}           The HSL representation
+ */
+ function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, l];
+}
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }
